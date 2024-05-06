9da01c4f88943b8cbf6a090b277b15be4ade3610
Ashton Xiong <ashtonxiong12@gmail.com>
2024-05-05 19:55:21 -0700

    fix mask

--- main.py
+++ main.py
@@ -9,7 +9,5 @@ async def on_start_up():
     print("Bot Initialized and Ready!")
 
 # Token for running
-token = "MTIzNTgwNjQwNDY4Njc3ODM2OA.GhvxAX.xFDx2l_MTii62KLA9Xy2hDbVewwnYl8r4pfJEo"
-secret = token.encode('utf-8')
-token = secret.decode('utf-8')
-bot.run(token)
+# In the parenthesisof bot.run add: "MTIzNTgwNjQwNDY4Njc3ODM2OA.GhvxAX.xFDx2l_MTii62KLA9Xy2hDbVewwnYl8r4pfJEo"
+bot.run()
