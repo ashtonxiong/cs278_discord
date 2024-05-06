fcf9685f0b44fc66a14c004e86b449d4727e4837
Ashton Xiong <ashtonxiong12@gmail.com>
2024-05-05 19:53:20 -0700

    token hidden

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ async def on_start_up():
     print("Bot Initialized and Ready!")
 
 # Token for running
-bot.run("MTIzNTgwNjQwNDY4Njc3ODM2OA.GhvxAX.xFDx2l_MTii62KLA9Xy2hDbVewwnYl8r4pfJEo")
+token = "MTIzNTgwNjQwNDY4Njc3ODM2OA.GhvxAX.xFDx2l_MTii62KLA9Xy2hDbVewwnYl8r4pfJEo"
+secret = token.encode('utf-8')
+token = secret.decode('utf-8')
+bot.run(token)
